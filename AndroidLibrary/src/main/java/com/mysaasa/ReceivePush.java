a301bb21fc1064dbcdbca6e3f01d54139866ac76
adamhammer2@gmail.com <Iluv5u5h1>
2016-04-04 18:34:29 -0700

    push now works well, with notification

--- AndroidLibrary/src/main/java/com/mysaasa/ReceivePush.java
+++ AndroidLibrary/src/main/java/com/mysaasa/ReceivePush.java
@@ -1,15 +1,20 @@
 package com.mysaasa;
 
 import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+import com.mysaasa.ui.ActivityMain;
 import com.mysaasa.ui.SideNavigationCompatibleActivity;
 import com.mysassa.R;
 import com.mysassa.api.MySaasaClient;
 import com.mysassa.api.messages.ThreadUpdatedPushMessage;
+import com.mysassa.api.model.Category;
 
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -47,18 +52,11 @@ public class ReceivePush extends BroadcastReceiver {
 
     private void HandleNewMessage(Context context, MySaasaClient s) {
         if (s!=null) {
-            s.getMessagesManager().getMessageCount()
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeOn(Schedulers.newThread())
-                    .subscribe(getMessageCountResponse -> {
-                        Toast.makeText(MySaasaApplication.getInstance(), "blah ", Toast.LENGTH_SHORT).show();
-                    });
-
             s.bus.post(new PushNotifiedNewMessage());
         }
 
 
-        if (!SideNavigationCompatibleActivity.isInForeground()) {
+        if (!SideNavigationCompatibleActivity.isInForeground() || 1 == 1) {
             Notification.Builder mBuilder =
                     new Notification.Builder(context)
                             .setSmallIcon(R.drawable.ic_app)
@@ -66,8 +64,9 @@ public class ReceivePush extends BroadcastReceiver {
                             .setContentText("Click to goto your inbox.");
 
             //Switch to goto Application
-            /*
-            Intent resultIntent = new Intent(context, ActivityMessages.class);
+
+            Intent resultIntent = new Intent(context, ActivityMain.class);
+            resultIntent.putExtra("category", new Category("Messages"));
 
             TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
             stackBuilder.addParentStack(ActivityMain.class);
@@ -80,10 +79,8 @@ public class ReceivePush extends BroadcastReceiver {
             mBuilder.setContentIntent(resultPendingIntent);
             NotificationManager mNotificationManager =
                     (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
             mNotificationManager.notify(5005, mBuilder.build());
 
-*/
         }
     }
 
