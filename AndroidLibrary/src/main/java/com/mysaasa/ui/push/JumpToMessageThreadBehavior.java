f46792b209ba60d6cb8c40ff956a392c1acc7e3b
adamhammer2@gmail.com <Iluv5u5h1>
2016-05-23 15:41:37 -0700

    Notification works now

--- AndroidLibrary/src/main/java/com/mysaasa/ui/push/JumpToMessageThreadBehavior.java
+++ AndroidLibrary/src/main/java/com/mysaasa/ui/push/JumpToMessageThreadBehavior.java
@@ -1,6 +1,7 @@
 package com.mysaasa.ui.push;
 
 import android.app.Activity;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.google.common.eventbus.Subscribe;
@@ -10,6 +11,7 @@ import com.mysaasa.ReceiveGCMPush;
 import com.mysaasa.api.model.Message;
 import com.mysaasa.api.model.PushMessageModel;
 import com.mysaasa.ui.ActivityChat;
+import com.mysassa.R;
 
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 
@@ -26,14 +28,19 @@ public class JumpToMessageThreadBehavior {
     @Subscribe
     public void onNewMessageEvent(Envelope<ReceiveGCMPush.PushMessage, PushMessageModel> message) {
         ReceiveGCMPush.PushMessage msg = message.open();
-        TextView v = new TextView(activity);
+        ViewGroup v = (ViewGroup) activity.getLayoutInflater().inflate(R.layout.crouton_new_message,null);
+
         Message data = message.getObject().getMessage();
-        v.setText("New Message Received: "+data.toString());
+        TextView title = (TextView) v.findViewById(R.id.title);
+        TextView body = (TextView) v.findViewById(R.id.body);
+        title.setText(data.title);
+        body.setText(data.body);
         v.setOnClickListener(v1 -> {
             ActivityChat.StartChat(activity, data);
         });
 
         Crouton.make(activity, v).show();
+
     }
 
     public final void start() {
